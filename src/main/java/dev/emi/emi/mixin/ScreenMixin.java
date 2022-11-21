64f00ff9ca4e4e096a96be7dda81d7d197ca2917
SFort <github@ssf.tf>
2022-11-21 07:22:37 +0100

    Fix /unascribed/fabrication#573

--- src/main/java/dev/emi/emi/mixin/ScreenMixin.java
+++ src/main/java/dev/emi/emi/mixin/ScreenMixin.java
@@ -1,21 +1,38 @@
 package dev.emi.emi.mixin;
 
-import java.util.List;
-
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
 import dev.emi.emi.EmiClient;
+import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.tooltip.TooltipComponent;
 import net.minecraft.client.util.math.MatrixStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+import java.util.List;
 
 @Mixin(Screen.class)
 public class ScreenMixin {
+
+	@Shadow
+	protected TextRenderer textRenderer;
+
+	@ModifyVariable(at = @At("HEAD"), method = "renderTooltipFromComponents", argsOnly = true, ordinal = 0)
+	private int moveX(int x, MatrixStack matrices, List<TooltipComponent> components) {
+		if (EmiClient.shiftTooltipsLeft && !components.isEmpty()) {
+			int width = 0;
+			for (TooltipComponent tooltipComponent : components) {
+				int w = tooltipComponent.getWidth(this.textRenderer);
+				if (w > width) {
+					width = w;
+				}
+			}
+			return x - width - 16;
+		}
+		return x;
+	}
+	/* New Old solution. breaks fabrications old_tooltip
 	@Shadow
 	private int width;
 
@@ -36,7 +53,7 @@ public class ScreenMixin {
 			this.width = this.emi$originalWidth;
 		}
 	}
-
+	 */
 	/* Old, different solution. Not sure if this is less hacky, or more, but it breaks botania's tooltip components
 	@Unique
 	private int emi$tooltipLeftMutation;
