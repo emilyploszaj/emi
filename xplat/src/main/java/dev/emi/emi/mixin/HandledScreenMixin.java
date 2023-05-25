d5b26bc04af3f894b507473556bbf0a83fe95bc9
emilyploszaj <emilyploszaj@gmail.com>
2023-05-24 20:57:44 -0500

    Use the EmiPort shader method

--- xplat/src/main/java/dev/emi/emi/mixin/HandledScreenMixin.java
+++ xplat/src/main/java/dev/emi/emi/mixin/HandledScreenMixin.java
@@ -5,12 +5,13 @@ import org.spongepowered.asm.mixin.Intrinsic;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.At.Shift;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 
+import dev.emi.emi.EmiPort;
 import dev.emi.emi.api.stack.EmiStack;
 import dev.emi.emi.screen.EmiScreen;
 import dev.emi.emi.screen.EmiScreenManager;
@@ -20,7 +21,6 @@ import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.client.gui.screen.recipebook.RecipeBookProvider;
-import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.util.Window;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.screen.slot.Slot;
@@ -61,7 +61,7 @@ public abstract class HandledScreenMixin extends Screen implements EmiScreen {
 		viewStack.push();
 		viewStack.translate(-x, -y, 0.0);
 		RenderSystem.applyModelViewMatrix();
-		RenderSystem.setShader(GameRenderer::getPositionTexShader);
+		EmiPort.setPositionTexShader();
 		EmiScreenManager.render(matrices, mouseX, mouseY, delta);
 		viewStack.pop();
 		RenderSystem.applyModelViewMatrix();
