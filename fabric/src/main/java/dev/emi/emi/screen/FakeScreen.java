594553f0f20a698aba118bef63495234185f1d2e
emilyploszaj <emilyploszaj@gmail.com>
2023-06-20 19:00:52 -0500

    Fabric legacy tooltip components

--- fabric/src/main/java/dev/emi/emi/screen/FakeScreen.java
+++ fabric/src/main/java/dev/emi/emi/screen/FakeScreen.java
@@ -1,10 +1,14 @@
 package dev.emi.emi.screen;
 
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.reflect.Method;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 import dev.emi.emi.EmiPort;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.tooltip.TooltipComponent;
@@ -13,6 +17,20 @@ import net.minecraft.item.ItemStack;
 
 public class FakeScreen extends Screen {
 	public static final FakeScreen INSTANCE = new FakeScreen();
+	private static MethodHandle handle;
+
+	static {
+		try {
+			String name = FabricLoader.getInstance().getMappingResolver()
+				.mapMethodName("intermediary", "net.minecraft.class_437", "method_32635",
+					"(Ljava/util/List;Lnet/minecraft/class_5632;)V");
+			Method m = Screen.class.getDeclaredMethod(name, List.class, TooltipData.class);
+			m.setAccessible(true);
+			handle = MethodHandles.lookup().unreflect(m);
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+	}
 
 	protected FakeScreen() {
 		super(EmiPort.literal(""));
@@ -29,7 +47,7 @@ public class FakeScreen extends Screen {
 		Optional<TooltipData> data = stack.getTooltipData();
 		if (data.isPresent()) {
 			try {
-				list.add(TooltipComponent.of(data.get()));
+				handle.invokeWithArguments(list, data.get());
 			} catch (Throwable e) {
 				e.printStackTrace();
 			}
