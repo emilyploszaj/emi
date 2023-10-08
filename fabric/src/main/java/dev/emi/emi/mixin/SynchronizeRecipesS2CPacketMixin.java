a784227641ebac70ae3840a16c0692ba2fb54d0b
emilyploszaj <emilyploszaj@gmail.com>
2023-10-07 23:05:35 -0500

    Revert 1.20.2 specific change

--- fabric/src/main/java/dev/emi/emi/mixin/SynchronizeRecipesS2CPacketMixin.java
+++ fabric/src/main/java/dev/emi/emi/mixin/SynchronizeRecipesS2CPacketMixin.java
@@ -10,15 +10,14 @@ import dev.emi.emi.runtime.EmiLog;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.network.packet.s2c.play.SynchronizeRecipesS2CPacket;
 import net.minecraft.recipe.Recipe;
-import net.minecraft.recipe.RecipeEntry;
 import net.minecraft.util.Identifier;
 
 @Mixin(SynchronizeRecipesS2CPacket.class)
 public class SynchronizeRecipesS2CPacketMixin {
 	
 	@Inject(at = @At("RETURN"), method = "readRecipe", locals = LocalCapture.CAPTURE_FAILHARD)
-	private static void readRecipe(PacketByteBuf buf, CallbackInfoReturnable<RecipeEntry<Recipe<?>>> info, Identifier serializer, Identifier id) {
-		RecipeEntry<Recipe<?>> recipe = info.getReturnValue();
+	private static void readRecipe(PacketByteBuf buf, CallbackInfoReturnable<Recipe<?>> info, Identifier serializer, Identifier id) {
+		Recipe<?> recipe = info.getReturnValue();
 		if (recipe == null) {
 			EmiLog.error("Recipe " + id + " was deserialized as null using recipe serializer " + serializer
 				+ ", this is breaking vanilla recipe sync, and will prevent EMI from loading recipes");
