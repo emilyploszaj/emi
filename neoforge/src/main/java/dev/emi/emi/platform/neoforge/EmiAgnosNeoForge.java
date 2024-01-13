1e3015928eb7711b97bc8bdacfdaac59bd61be56
Sisby folk <55819817+sisby-folk@users.noreply.github.com>
2024-01-14 05:59:22 +1100

    Mod name tooltips: match kebab case mod IDs, fall back to friendly namespaces (#399)
    * match kebab case mod IDs for mod names
    
    * fall back to friendly namespace for mod names
    players that need IDs can use F3+H
    
    * suppress 1
    
    * suppress 2 forge edition
    
    * suppress 3 neo thank you morpheus

--- neoforge/src/main/java/dev/emi/emi/platform/neoforge/EmiAgnosNeoForge.java
+++ neoforge/src/main/java/dev/emi/emi/platform/neoforge/EmiAgnosNeoForge.java
@@ -11,6 +11,7 @@ import net.minecraft.item.PotionItem;
 import net.minecraft.registry.entry.RegistryEntry;
 import net.neoforged.neoforge.client.ClientHooks;
 import net.neoforged.neoforge.common.CommonHooks;
+import org.apache.commons.lang3.text.WordUtils;
 import org.objectweb.asm.Type;
 
 import com.google.common.collect.Lists;
@@ -70,6 +71,7 @@ public class EmiAgnosNeoForge extends EmiAgnos {
 		return true;
 	}
 
+	@SuppressWarnings("deprecation")
 	@Override
 	protected String getModNameAgnos(String namespace) {
 		if (namespace.equals("c")) {
@@ -79,7 +81,11 @@ public class EmiAgnosNeoForge extends EmiAgnos {
 		if (container.isPresent()) {
 			return container.get().getModInfo().getDisplayName();
 		}
-		return namespace;
+		container = ModList.get().getModContainerById(namespace.replace('_', '-'));
+		if (container.isPresent()) {
+			return container.get().getModInfo().getDisplayName();
+		}
+		return WordUtils.capitalizeFully(namespace.replace('_', ' '));
 	}
 
 	@Override
