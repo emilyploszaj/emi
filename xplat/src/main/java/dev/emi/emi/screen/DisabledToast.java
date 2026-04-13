58741097963019a883e2d7843152995a1781aca3
ConcerroX <concerrox@outlook.com>
2026-04-13 22:56:07 +0800

    Remove disabled toast temporarily;

--- xplat/src/main/java/dev/emi/emi/screen/DisabledToast.java
+++ xplat/src/main/java/dev/emi/emi/screen/DisabledToast.java
@@ -3,6 +3,8 @@ package dev.emi.emi.screen;
 import dev.emi.emi.EmiPort;
 import dev.emi.emi.config.EmiConfig;
 import dev.emi.emi.runtime.EmiDrawContext;
+
+import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.toast.Toast;
 import net.minecraft.client.toast.ToastManager;
@@ -11,16 +13,47 @@ import net.minecraft.util.Identifier;
 public class DisabledToast implements Toast {
 	private static final Identifier TEXTURE = EmiPort.id("toast/advancement");
 
-	@Override
-	public Visibility draw(DrawContext raw, ToastManager manager, long time) {
-		EmiDrawContext context = EmiDrawContext.wrap(raw);
-		context.resetColor();
-		raw.drawGuiTexture(TEXTURE, 0, 0, this.getWidth(), this.getHeight());
-		context.drawCenteredText(EmiPort.translatable("emi.disabled"), getWidth() / 2, 7);
-		context.drawCenteredText(EmiConfig.toggleVisibility.getBindText(), getWidth() / 2, 18);
-		if (time > 8_000 || EmiConfig.enabled) {
-			return Visibility.HIDE;
-		}
-		return Visibility.SHOW;
-	}
+//	@Override
+//	public Visibility draw(DrawContext raw, ToastManager manager, long time) {
+//		EmiDrawContext context = EmiDrawContext.wrap(raw);
+//		context.resetColor();
+//		raw.drawGuiTexture(TEXTURE, 0, 0, this.getWidth(), this.getHeight());
+//		context.drawCenteredText(EmiPort.translatable("emi.disabled"), getWidth() / 2, 7);
+//		context.drawCenteredText(EmiConfig.toggleVisibility.getBindText(), getWidth() / 2, 18);
+//		if (time > 8_000 || EmiConfig.enabled) {
+//			return Visibility.HIDE;
+//		}
+//		return Visibility.SHOW;
+//	}
+
+    @Override
+    public Visibility getVisibility() {
+//        EmiDrawContext context = EmiDrawContext.wrap(raw);
+//        context.resetColor();
+//        raw.drawGuiTexture(TEXTURE, 0, 0, this.getWidth(), this.getHeight());
+//        context.drawCenteredText(EmiPort.translatable("emi.disabled"), getWidth() / 2, 7);
+//        context.drawCenteredText(EmiConfig.toggleVisibility.getBindText(), getWidth() / 2, 18);
+//        if (time > 8_000 || EmiConfig.enabled) {
+//            return Visibility.HIDE;
+//        }
+        return Visibility.SHOW;
+    }
+
+    @Override
+    public void update(ToastManager manager, long time) {
+
+    }
+
+    @Override
+    public void draw(DrawContext context, TextRenderer textRenderer, long startTime) {
+//        EmiDrawContext context = EmiDrawContext.wrap(raw);
+//        context.resetColor();
+//        raw.drawGuiTexture(TEXTURE, 0, 0, this.getWidth(), this.getHeight());
+//        context.drawCenteredText(EmiPort.translatable("emi.disabled"), getWidth() / 2, 7);
+//        context.drawCenteredText(EmiConfig.toggleVisibility.getBindText(), getWidth() / 2, 18);
+//        if (time > 8_000 || EmiConfig.enabled) {
+//            return Visibility.HIDE;
+//        }
+//        return Visibility.SHOW;
+    }
 }
