8ab1ee77d9a6488fe3c47f9fe1d9ecbcec912158
emilyploszaj <emilyploszaj@gmail.com>
2026-08-18 00:01:17 -0500

    Fix imports

--- xplat/src/main/java/dev/emi/emi/screen/widget/config/GroupNameWidget.java
+++ xplat/src/main/java/dev/emi/emi/screen/widget/config/GroupNameWidget.java
@@ -9,6 +9,7 @@ import dev.emi.emi.runtime.EmiDrawContext;
 import dev.emi.emi.screen.widget.config.ListWidget.Entry;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.Element;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.Text;
 
 public class GroupNameWidget extends Entry {
